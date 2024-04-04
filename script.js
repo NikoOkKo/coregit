93a8db184e62d3b1875db33a7e70cdaee0c5cf4f
Daniel Levican <daniel.levican.imilmaqui@institutocomercialosorno.cl>
2024-04-04 10:14:30 -0400

    coregit

---
+++ script.js
@@ -0,0 +1,12 @@
+document.getElementById('buenosAires').addEventListener("click",function(){
+    window.location.href="BuenosAires.html";
+})
+document.getElementById('ciudadDeMexico').addEventListener("click",function(){
+    window.location.href="CiudadDeMexico.html";
+})
+document.getElementById('quito').addEventListener("click",function(){
+    window.location.href="Quito.html";
+})
+document.getElementById('saoPaulo').addEventListener("click",function(){
+    window.location.href="SaoPaulo.html";
+})
